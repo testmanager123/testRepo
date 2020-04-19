4394bc6042bc600ce7fcc1ebea5dbfbc0f4fea0a
testmanager123 <srinivasavy@gmail.com>
2020-04-19 14:04:40 +0530

    second commit

---
+++ LakshmiSel/GitUpload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakshmiSel
+{
+    class GitUpload
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("this is up loading to git");
+        }
+    }
+}
