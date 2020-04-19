069a60f66c8bce83066082fb3a8bfbf964c0481a
testmanager123 <srinivasavy@gmail.com>
2020-04-19 13:31:13 +0530

    local commit

---
+++ LakshmiSel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakshmiSel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hai this is testing ppurpose");
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
